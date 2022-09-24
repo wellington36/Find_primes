830e6f642270f9ac7458d78df6744967296b5fd6
wellington36 <wellington.71319@gmail.com>
2022-09-24 15:24:02 -0300

    Initial commit

---
+++ find_primes.py
@@ -0,0 +1,31 @@
+import time
+
+def find_prime(n):
+    MIN_NUMBER = 2
+    MAX_NUMBER = n
+    primeArray = []
+    
+    for i in range(MIN_NUMBER,MAX_NUMBER):
+        result = False
+        temp = int(i**0.5)
+        
+        for prime in primeArray[:temp]:
+            if (i%prime == 0):
+                result = True
+                break
+            
+        if not result: primeArray.append(i)
+
+    return primeArray
+
+
+if __name__ == '__main__':
+    N = 10000000
+
+    start = time.time()
+
+    primes = find_prime(N)
+
+    end2 = time.time()
+
+    print(f"Wellington's algorithm: {end2 - end1}")
